6745a1ea779106b08255354f2468607d5b80d7da
Carlos Carrillo-Gallegos <90050144+CarlosECarrilloGallegos@users.noreply.github.com>
2024-05-14 11:16:27 -0400

    Create cams_pm25_daily.py

---
+++ Datasets/cams/cams_pm25_daily.py
@@ -0,0 +1,39 @@
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+import glob
+import scipy
+import datetime
+#cams
+time = ['2003','2004','2005','2006','2007','2008','2009',
+        '2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021']
+
+cams = xr.open_dataset('/data0/shr/bne/data/raw/pm25/cams/cams_pm25_daymean_2003_2022.nc')
+for i in time:
+    cams_slice = cams.sel(time=slice(i + '-01-01T10:00:00.000000000',i + '-12-31T10:00:00.000000000'))
+    cams_df = cams_slice.to_dataframe()
+    cams_df = cams_df.reset_index()
+    cams_df = cams_df.drop('bnds',axis=1).drop('time_bnds',axis=1).rename(columns={'pm2p5':'cams'})
+    cams_df['cams'] = cams_df['cams']*(1e9)
+    #remove unwanted column
+    cams_df = cams_df[['latitude','longitude','time','cams']]
+    #rename columns
+    cams_df = cams_df.rename(columns = {'longitude':'lon','latitude':'lat','time':'date'})
+    
+    #Convert longitude
+    cams_lon = (np.array(cams_df['lon']))
+    j = 0
+    for k in cams_lon:
+        if k >= 180:
+            cams_lon[j] = k - 360
+        j+=1
+
+    cams_df['lon'] = cams_lon
+
+    cams_df['date'] = pd.to_datetime(cams_df['date']).dt.date
+    
+    cams_df = cams_df.drop_duplicates()
+    
+    cams_df.to_csv('/data0/shr/bne/data/pm25/base_models/daily/formatted/cams/cams_pm25_' + i + '_daily.csv',index=False)
+  
