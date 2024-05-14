fa456b4b8c77dafcc75aa949b7fbd4acaae7c072
Carlos Carrillo-Gallegos <90050144+CarlosECarrilloGallegos@users.noreply.github.com>
2024-05-14 10:23:37 -0400

    Create equates_no2_daily.py

---
+++ Datasets/equates/equates_no2_daily.py
@@ -0,0 +1,38 @@
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+import glob
+import scipy
+import datetime
+import gc
+#EQUATES
+time = ['2002','2003','2004','2005','2006','2007','2008','2009',
+        '2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
+
+for j in time:
+
+    csvs = sorted(glob.glob('/data0/shr/bne/data/raw/multi/equates/'+j +'/*.csv'))
+    equates_year = []
+
+    for i in csvs:
+        equates_init = pd.read_csv(i,skiprows=[3]).reset_index()
+        new_header = equates_init.iloc[0]
+        equates = equates_init[1:]
+        equates.columns = new_header
+        equates = equates[['longitude','latitude','date','NO2_AVG']]
+    
+        equates_year.append(equates)
+    
+    equates_final = pd.concat(equates_year)
+    
+    equates_final = equates_final.rename(columns={'longitude':'lon','latitude':'lat','NO2_AVG':'equates'})
+    
+    equates_final = equates_final[['lat','lon','date','equates']]
+    
+    equates_final = equates_final.reset_index()[['lat','lon','date','equates']]
+    
+    equates_final['date'] = pd.to_datetime(equates_final['date'])
+    
+    equates_final.to_csv('/data0/shr/bne/data/nox/daily/base_models/formatted/equates/equates_no2_' + j + '_daily.csv', index=False)
+    
